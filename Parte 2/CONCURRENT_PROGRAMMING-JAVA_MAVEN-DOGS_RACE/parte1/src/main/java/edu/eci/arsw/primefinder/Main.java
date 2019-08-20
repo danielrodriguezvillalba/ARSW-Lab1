d2460d1b6d3f337073ca608c5e105e19f0dd135f
Daniel Rodriguez <daniel.rodriguez-vi@mail.escuelaing.edu.co>
2019-08-19 19:49:17 -0500

    Parte 2 finalizacion punto 1

--- Parte 2/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ Parte 2/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,35 @@
 package edu.eci.arsw.primefinder;
 
+import java.io.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+		PrimeFinderThread pft1=new PrimeFinderThread(0, 10000000);
+		PrimeFinderThread pft2=new PrimeFinderThread(10000000, 20000000);
+		PrimeFinderThread pft3=new PrimeFinderThread(20000000, 30000000);
+		pft1.start();
+		pft2.start();
+		pft3.start();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		new java.util.Timer().schedule( 
+		        new java.util.TimerTask() {
+		            @Override
+		            public void run() {
+		                try {
+		                	pft1.suspend();
+							pft2.suspend();
+							pft3.suspend();
+							while(br.read() != '\n') {
+								br.read();
+							}
+							pft1.resume();
+							pft2.resume();
+							pft3.resume();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+		            }
+		        },5000);
 	}
-	
 }
